208dcd511a9198c5eb503a3512f25348819c78b6
Segun Ajibade <65359479+sajibade43@users.noreply.github.com>
2022-03-14 23:49:40 -0400

    Update QuestionViewController.swift

--- PersonalityQuiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -2,9 +2,7 @@
 //  QuestionViewController.swift
 //  PersonalityQuiz
 //
-//  Created by Segun on 14/3/22.
-//  Copyright © 2022 Segun. All rights reserved.
-//
+//  Created by Segun on 3/14/22.
 
 import UIKit
 
@@ -198,14 +196,4 @@ class QuestionViewController: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
